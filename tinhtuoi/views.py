4675b02ce63fbbdc353533259bcbec7dc6189353
Vu Long <lumosnysm@gmail.com>
2022-10-30 23:13:09 +0700

    update

--- tinhtuoi/views.py
+++ tinhtuoi/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from datetime import date
