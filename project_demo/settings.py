a5c8309a02a03cea0f29c673650f5f5f0a76a121
Vu Long <lumosnysm@gmail.com>
2022-10-30 22:45:23 +0700

    db prod

--- project_demo/settings.py
+++ project_demo/settings.py
@@ -82,13 +82,15 @@ WSGI_APPLICATION = 'project_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
