3ff27b027f57a8ef0fba28836f335d5622927cd4
Vu Long <lumosnysm@gmail.com>
2022-10-30 21:32:31 +0700

    add admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Feedback, GenDoc
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Feedback)
+admin.site.register(GenDoc)
